a30890e44e60840b037009b10873581828b87dc3
dj <futurecrew@gmail.com>
2015-12-13 15:07:54 +0900

    Android app

---
+++ app/src/main/java/com/example/dj/deepcamera/CameraPreview.java
@@ -0,0 +1,217 @@
+package com.example.dj.deepcamera;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private List<Camera.Size> mSupportedPreviewSizes;
+    private Camera.Size mPreviewSize;
+    private Context mContext;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mContext = context;
+        mCamera = camera;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        // supported preview sizes
+        mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+        for(Camera.Size str: mSupportedPreviewSizes)
+            Log.e(VIEW_LOG_TAG, str.width + "/" + str.height);
+
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // create the surface and start camera preview
+            if (mCamera != null) {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }
+        } catch (IOException e) {
+            Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void refreshCamera(Camera camera) {
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            Log.e(VIEW_LOG_TAG, "preview surface does not exist");
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setCamera(camera);
+        try {
+            Log.e(VIEW_LOG_TAG, "mPreviewSize = " + mPreviewSize.width + "/" + mPreviewSize.height);
+
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+            parameters.setFocusMode(parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            mCamera.setParameters(parameters);
+
+            mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+                public void onPreviewFrame(byte[] data, Camera camera) {
+                    //Log.d("MyCam", "setPreviewCallback");
+
+                    if (true)
+                        return;
+
+                    Camera.Parameters params = mCamera.getParameters();
+                    int w = params.getPreviewSize().width;
+                    int h = params.getPreviewSize().height;
+                    int format = params.getPreviewFormat();
+                    YuvImage image = new YuvImage(data, format, w, h, null);
+
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    Rect area = new Rect(0, 0, w, h);
+                    image.compressToJpeg(area, 50, out);
+                    Bitmap captureImg = BitmapFactory.decodeByteArray(out.toByteArray(), 0, out.size());
+
+
+                    FileOutputStream fOut = null;
+                    try {
+                        String path = Environment.getExternalStorageDirectory().toString() + "/data/camera";
+                        File file = new File(path, System.currentTimeMillis() + ".jpg"); // the File to save to
+                        fOut = new FileOutputStream(file);
+                        captureImg.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            if (fOut != null) {
+                                fOut.close();
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                }
+            });
+        } catch (Exception e) {
+            Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        refreshCamera(mCamera);
+    }
+
+    public void setCamera(Camera camera) {
+        //method to set a camera instance
+        mCamera = camera;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        // mCamera.release();
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+        final int rotation = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                width = 768;
+                height = 1082;
+                Log.d(VIEW_LOG_TAG, "ROTATION_0");
+                break;
+            case Surface.ROTATION_90:
+                width = 1024;
+                height = 768;
+                Log.d(VIEW_LOG_TAG, "ROTATION_90");
+                break;
+            case Surface.ROTATION_180:
+                width = 1024;
+                height = 768;
+                Log.d(VIEW_LOG_TAG, "ROTATION_180");
+                break;
+            default:
+                width = 1024;
+                height = 768;
+                Log.d(VIEW_LOG_TAG, "default");
+                break;
+        }
+
+        setMeasuredDimension(width, height);
+
+        if (mSupportedPreviewSizes != null) {
+            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
+        }
+    }
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+}
