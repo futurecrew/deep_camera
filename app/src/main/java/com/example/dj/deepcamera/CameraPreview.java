19875c3106c432c9746a64b324cabd7dfed18981
dj <futurecrew@gmail.com>
2016-01-14 14:45:17 +0900

    fix bugs

--- app/src/main/java/com/example/dj/deepcamera/CameraPreview.java
+++ app/src/main/java/com/example/dj/deepcamera/CameraPreview.java
@@ -21,9 +21,12 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -32,11 +35,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
@@ -182,6 +188,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             parameters.setFocusMode(parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
             mCamera.setParameters(parameters);
 
+            /*
             mCamera.setPreviewCallback(new Camera.PreviewCallback() {
                 public void onPreviewFrame(byte[] data, Camera camera) {
                     try {
@@ -204,9 +211,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
                     }
                 }
             });
+            */
 
 
-            /*
             mCamera.setPreviewCallback(new Camera.PreviewCallback() {
                 public void onPreviewFrame(byte[] data, Camera camera) {
                     //Log.d("MyCam", "setPreviewCallback");
@@ -214,21 +221,22 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
                     //if (true)
                     //    return;
 
-                    Camera.Parameters params = mCamera.getParameters();
-                    //int w = params.getPreviewSize().width;
-                    //int h = params.getPreviewSize().height;
-                    int format = params.getPreviewFormat();
-                    YuvImage image = new YuvImage(data, format, mPictureSize.width, mPictureSize.height, null);
+                    Camera.Parameters parameters = camera.getParameters();
+                    Camera.Size previewSize = parameters.getPreviewSize();
+                    Camera.Size pictureSize = parameters.getPictureSize();
+                    YuvImage image = new YuvImage(data, parameters.getPreviewFormat(),
+                            previewSize.width, previewSize.height, null);
 
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    Rect area = new Rect(0, 0, mPictureSize.width, mPictureSize.height);
+                    Rect area = new Rect(0, 0, image.getWidth(), image.getHeight());
                     image.compressToJpeg(area, 50, out);
                     //Bitmap captureImg = BitmapFactory.decodeByteArray(out.toByteArray(), 0, out.size());
 
 
                     FileOutputStream fOut = null;
                     try {
-                        File file = new File(img_folder, System.currentTimeMillis() + ".jpg"); // the File to save to
+                        String fileName = System.currentTimeMillis() + ".jpg";
+                        File file = new File(img_folder, fileName); // the File to save to
                         fOut = new FileOutputStream(file);
                         //ByteArrayOutputStream bos = new ByteArrayOutputStream();
                         //captureImg.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
@@ -240,9 +248,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
                         String url = "http://" + mServerIp + ":" + mServerPort;
 
-                        Log.d(VIEW_LOG_TAG, "calling ProcessHttpTask() mPictureSize.width:" + mPictureSize.width + ", mPictureSize.height:" + mPictureSize.height);
+                        Log.d(VIEW_LOG_TAG, "calling ProcessHttpTask() " + image.getWidth() + ", " + image.getHeight());
 
-                        new ProcessHttpTask(url, mMode, buffer).execute();
+                        new ProcessHttpTask(url, mMode, fileName, buffer).execute();
 
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -258,7 +266,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
                 }
             });
-            */
+
 
         } catch (Exception e) {
             Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
@@ -288,20 +296,34 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         //int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
         //int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
 
-        int temp = 0;
+        int newDisplayWidth = 0;
+        int newDisplayHeight = 0;
 
         if (mSupportedPreviewSizes != null) {
             final int rotation = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
             switch (rotation) {
+                case Surface.ROTATION_0:
+                    newDisplayWidth = mDisplayWidth;
+                    newDisplayHeight = mDisplayHeight;
+                    Log.d(VIEW_LOG_TAG, "ROTATION_0");
+                    break;
                 case Surface.ROTATION_90:
-                case Surface.ROTATION_270:
-                    temp = mDisplayWidth;
-                    mDisplayWidth = mDisplayHeight;
-                    mDisplayHeight = temp;
+                    newDisplayWidth = mDisplayHeight;
+                    newDisplayHeight = mDisplayWidth;
                     Log.d(VIEW_LOG_TAG, "ROTATION_90");
                     break;
+                case Surface.ROTATION_180:
+                    newDisplayWidth = mDisplayWidth;
+                    newDisplayHeight = mDisplayHeight;
+                    Log.d(VIEW_LOG_TAG, "ROTATION_180");
+                    break;
+                case Surface.ROTATION_270:
+                    newDisplayWidth = mDisplayHeight;
+                    newDisplayHeight = mDisplayWidth;
+                    Log.d(VIEW_LOG_TAG, "ROTATION_270");
+                    break;
             }
-            setMeasuredDimension(mDisplayWidth, mDisplayHeight);
+            setMeasuredDimension(newDisplayWidth, newDisplayHeight);
         }
     }
 
@@ -344,14 +366,17 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 class ProcessHttpTask extends AsyncTask<Void, Void, Void> {
     private String url;
     private String mode;
+    private String fileName;
     private byte[] data;
 
-    public ProcessHttpTask(String url, String mode, byte[] data) {
+    public ProcessHttpTask(String url, String mode, String fileName, byte[] data) {
         this.url = url;
         this.mode = mode;
+        this.fileName = fileName;
         this.data = data;
     }
 
+    /*
     @Override
     protected Void doInBackground(Void... params) {
         OutputStream outputToServer = null;
@@ -407,7 +432,238 @@ class ProcessHttpTask extends AsyncTask<Void, Void, Void> {
         }
         return null;
     }
+*/
 
+    @Override
+    protected Void doInBackground(Void... params) {
+        OutputStream outputToServer = null;
+
+        Log.d("View", "doInBackground");
+
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        String Tag="fSnd";
+
+        try {
+
+            String urlParameters  = "param1=a&param2=b&param3=c";
+            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+            int    postDataLength = postData.length;
+            //String request        = "http://www.google.com";
+            String request        = "http://192.168.0.18:8080/update/";
+            URL    url            = new URL( request );
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            //conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            conn.setUseCaches(false);
+
+
+            /*
+            StringBuilder result = new StringBuilder();
+            result.append("name=");
+            result.append(this.fileName);
+            result.append("&data=");
+
+            OutputStream os = conn.getOutputStream();
+            os.write( result.toString().getBytes() );
+            os.write(this.data);
+            os.flush();
+            os.close();
+            */
+
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"name\""+ lineEnd);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(this.fileName);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+            dos.writeBytes("Content-Disposition: form-data; name=\"data\";filename=\"" + this.fileName +"\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            Log.e(Tag,"Headers are written");
+
+            dos.write(this.data);
+
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            dos.flush();
+
+
+
+
+
+            InputStream is      = null;
+            BufferedReader in   = null;
+            String data         = "";
+
+            is  = conn.getInputStream();
+            in  = new BufferedReader(new InputStreamReader(is), 8 * 1024);
+
+            String line = null;
+            StringBuffer buff   = new StringBuffer();
+
+            while ( ( line = in.readLine() ) != null )
+            {
+                buff.append(line + "\n");
+            }
+            data    = buff.toString().trim();
+
+            Log.d("View", "data : " + data);
+
+            /*
+            String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
+            //String charset = "ISO-8859-1";
+
+            HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            //connection.setRequestProperty("Content-Type", "image/jpeg");
+            //connection.setRequestProperty("Accept-Charset", charset);
+            //connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            StringBuilder result = new StringBuilder();
+            result.append(this.fileName);
+            result.append("&data=");
+
+            OutputStream os = connection.getOutputStream();
+            //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, charset));
+            //writer.write(result.toString());
+            //writer.write(new String(data, charset));
+
+            //writer.flush();
+            //writer.close();
+
+            //os.write( result.toString().getBytes() );
+
+            String param = "xx=111&yy=222";
+            os.write( param.getBytes() );
+            //os.write(data);
+            os.flush();
+
+            os.close();
+
+            //connection.connect();
+            */
+
+            /*
+            String response = "";
+            int responseCode=connection.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else response = "";
+            */
+
+            //Log.d("View", "response : " + response);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (outputToServer != null) {
+                try {
+                    outputToServer.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+
+/*
+    @Override
+    protected Void doInBackground(Void... params) {
+        OutputStream outputToServer = null;
+
+        //Log.d("View", "doInBackground");
+
+        try {
+            Log.d("View", "this.url : " + this.url);
+
+            //URL url = new URL("http://www.naver.com");
+            URL url = new URL(this.url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            // 서버로부터 메세지를 받을 수 있도록 한다. 기본값은 true이다.
+            con.setDoInput(true);
+
+            // 헤더값을 설정한다.
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            // 전달 방식을 설정한다. POST or GET, 기본값은 GET 이다.
+            con.setRequestMethod("POST");
+
+            // 서버로 데이터를 전송할 수 있도록 한다. GET방식이면 사용될 일이 없으나, true로 설정하면 자동으로 POST로 설정된다. 기본값은 false이다.
+            con.setDoOutput(true);
+
+            // POST방식이면 서버에 별도의 파라메터값을 넘겨주어야 한다.
+            String param    = "ID=rQ+g4R8qmTlAey1Wn/PwUA==&cust_no=vBiSI2BWVsu6lK03U7dsfA==&prom_no=";
+            //String param    = "ID="+ JspUtil.urlEncode(sNetMableID)+"&cust_no="+ JspUtil.urlEncode(sEncCustNo)+"&prom_no="+prom_no;
+
+            OutputStream out_stream = con.getOutputStream();
+
+            out_stream.write( param.getBytes("UTF-8") );
+            out_stream.flush();
+            out_stream.close();
+
+
+
+
+            InputStream is      = null;
+            BufferedReader in   = null;
+            String data         = "";
+
+            is  = con.getInputStream();
+            in  = new BufferedReader(new InputStreamReader(is), 8 * 1024);
+
+            String line = null;
+            StringBuffer buff   = new StringBuffer();
+
+            while ( ( line = in.readLine() ) != null )
+            {
+                buff.append(line + "\n");
+            }
+            data    = buff.toString().trim();
+
+            Log.d("View", "data : " + data);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (outputToServer != null) {
+                try {
+                    outputToServer.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+*/
     private String getQuery(List<Pair> params) throws UnsupportedEncodingException
     {
         StringBuilder result = new StringBuilder();
